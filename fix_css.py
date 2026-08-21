5b6cdf42ace0cf46d5421c7b2961e4ebedbc77b7
codddeeerrr <pctarvis@gmail.com>
2026-08-21 16:45:58 +0530

    final ui

--- fix_css.py
+++
@@ -1,7 +0,0 @@
-with open('/home/harry/Projects/ctf/private/styles.css', 'r') as f:
-    content = f.read()
-
-content = content.replace('margin-top: 15px;', 'margin-top: 30px;')
-
-with open('/home/harry/Projects/ctf/private/styles.css', 'w') as f:
-    f.write(content)
--- get_diff.py
+++
@@ -1,7 +0,0 @@
-with open('/home/harry/Projects/ctf/private/dashboard.html.bak', 'r') as f:
-    text = f.read()
-
-start = text.find('<body>')
-end = text.find('<!-- USERS SECTION -->')
-if start != -1 and end != -1:
-    print(text[start:end])
